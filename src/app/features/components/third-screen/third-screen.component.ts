1cf00789a4ed384da93862994011160420173f83
Ana Beatriz Gonçalves <ana.goncalves@itsector.pt>
2023-07-31 12:39:17 +0100

    Added go back button in components.

--- src/app/features/components/third-screen/third-screen.component.ts
+++ src/app/features/components/third-screen/third-screen.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from 'src/app/shared/components/button/button.component';
 
 @Component({
   selector: 'app-third-screen',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './third-screen.component.html',
-  styleUrls: ['./third-screen.component.scss']
+  styleUrls: ['./third-screen.component.scss'],
 })
-export class ThirdScreenComponent {
-
-}
+export class ThirdScreenComponent {}
