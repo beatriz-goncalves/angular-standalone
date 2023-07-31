58c7fc4a0646c2119ab22a371a0a9bd640f95ce7
Ana Beatriz Gonçalves <ana.goncalves@itsector.pt>
2023-07-31 16:30:05 +0100

    Third screen to make use signals - signal, computed and effect. Within signal, use of set, mutate and update.

--- src/app/features/components/third-screen/third-screen.component.ts
+++ src/app/features/components/third-screen/third-screen.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, computed, effect } from '@angular/core';
+import { CommonModule, NgForOf } from '@angular/common';
 import { ButtonComponent } from 'src/app/shared/components/button/button.component';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-third-screen',
@@ -9,4 +10,28 @@ import { ButtonComponent } from 'src/app/shared/components/button/button.compone
   templateUrl: './third-screen.component.html',
   styleUrls: ['./third-screen.component.scss'],
 })
-export class ThirdScreenComponent {}
+export class ThirdScreenComponent implements OnInit {
+  counter = signal(0);
+  doubleCounter = computed(() => this.counter() * 2);
+  actions = signal<string[]>([]);
+
+  constructor() {
+    effect(() => {
+      console.log('COUNTER: ', this.counter());
+      console.log('DOUBLE COUNTER: ', this.doubleCounter());
+      console.log('ACTIONS: ', this.actions());
+    });
+  }
+
+  ngOnInit(): void {}
+
+  increment = () => {
+    this.counter.set(this.counter() + 1);
+    this.actions.mutate((oldActions) => oldActions.push('INCREMENT'));
+  };
+
+  decrement = () => {
+    this.counter.update((currentCounter) => currentCounter - 1);
+    this.actions.mutate((oldActions) => oldActions.push('DECREMENT'));
+  };
+}
